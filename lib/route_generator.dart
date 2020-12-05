6aa8a62df4d89a1f79c1b6c4b1cfd2931e4c64fe
sdoshi983 <sdoshi983@rku.ac.in>
2020-12-05 23:34:31 +0530

    stack's main page added

--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -2,6 +2,7 @@ import 'package:dsa_simulation/src/data_structures/linear_and_non_linear.dart';
 import 'src/data_structures/linear/linear.dart';
 import 'src/Data_Structures/Linear/array/array_pageview.dart';
 import 'package:flutter/material.dart';
+
 class RouteGenerator{
   static Route<dynamic> generateRoute(RouteSettings settings){
     final args = settings.arguments;
@@ -14,7 +15,6 @@ class RouteGenerator{
         return MaterialPageRoute(builder: (_)=>LinearDS());
       case '/ArrayPageView':
         return MaterialPageRoute(builder:(_)=>ArrayPageView());
-
       default:
       // If there is no such named route in the switch statement, e.g. /third
         return MaterialPageRoute(
