9bf790942075aa0f45822fb0c7eca0dade980f06
sdoshi983 <sdoshi983@rku.ac.in>
2020-12-09 15:50:29 +0530

    Linked list's path added in route generator

--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -1,5 +1,6 @@
 import 'package:dsa_simulation/src/Data_Structures/Linear/Stack/Introduction.dart';
 import 'package:dsa_simulation/src/Data_Structures/Linear/Stack/stack_mainPage.dart';
+import 'package:dsa_simulation/src/Data_Structures/Linear/linked_list/linked_list_main_page.dart';
 import 'package:dsa_simulation/src/data_structures/linear_and_non_linear.dart';
 import 'src/data_structures/linear/linear.dart';
 import 'src/Data_Structures/Linear/array/array_navigation_page.dart';
@@ -20,6 +21,8 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (_) => StackMainPage());
       case '/StackIntroduction':
         return MaterialPageRoute(builder: (_) => StackIntroduction());
+      case '/LinkedListMainPage':
+        return MaterialPageRoute(builder: (_) => LinkedListMainPage());
       default:
         // If there is no such named route in the switch statement, e.g. /third
         return MaterialPageRoute(
