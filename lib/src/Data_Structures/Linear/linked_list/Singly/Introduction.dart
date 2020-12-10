81a14e3a9bda67647343d32343abec2fc0709dd7
sdoshi983 <sdoshi983@rku.ac.in>
2020-12-10 15:32:13 +0530

    Arrows created between the nodes of the list

--- lib/src/Data_Structures/Linear/linked_list/Singly/Introduction.dart
+++ lib/src/Data_Structures/Linear/linked_list/Singly/Introduction.dart
@@ -1,5 +1,6 @@
 import 'package:dsa_simulation/src/Utilities/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:widget_arrows/widget_arrows.dart';
 
 import '../../../../constants.dart';
 
@@ -13,88 +14,105 @@ class _SinglyIntroductionState extends State<SinglyIntroduction> {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: appBar(context),
-      body: Container(
-        color: Colors.black,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              'Introduction to Singly Linked List',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            SizedBox(height: height * 0.05,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  height: height * 0.05,
-                  width: width * 0.2,
-                  child: Center(child: Text('Data')),
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    //borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-
-                Container(
-                  height: height * 0.05,
-                  width: width * 0.2,
-                  child: Center(child: Text('Data')),
-                  decoration: BoxDecoration(
-                    color: Colors.yellow,
-                    //borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-                Container(
-                  height: height * 0.05,
-                  width: width * 0.2,
-                  child: Center(child: Text('Data')),
-                  decoration: BoxDecoration(
-                    color: Colors.cyan,
-                    //borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-
-              ],
-            ),
-            Container(
-              height: height * 0.05,
-              width: width * 0.2,
-              child: Center(child: Text('Data')),
-              decoration: BoxDecoration(
-                color: Colors.pinkAccent,
-                //borderRadius: BorderRadius.circular(20),
+    return ArrowContainer(
+      child: Scaffold(
+        appBar: appBar(context),
+        body: Container(
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                'Introduction to Singly Linked List',
+                style: Theme.of(context).textTheme.headline6,
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      //reverseAnimation();
-                    });
-                  },
-                  child: Icon(Icons.backspace_sharp),
-                  color: kThemeColor,
+              SizedBox(height: height * 0.05,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ArrowElement(
+                    id: 'first',
+                    targetId: 'second',
+                    sourceAnchor: Alignment.centerRight,
+                    //targetAnchor: Alignment.centerLeft,
+                    child: Container(
+                      height: height * 0.05,
+                      width: width * 0.2,
+                      child: Center(child: Text('Data')),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        //borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+
+                  ArrowElement(
+                    id: 'second',
+                    targetId: 'third',
+                    sourceAnchor: Alignment.centerRight,
+
+                    child: Container(
+                      height: height * 0.05,
+                      width: width * 0.2,
+                      child: Center(child: Text('Data')),
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        //borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  ArrowElement(
+                    id: 'third',
+                    child: Container(
+                      height: height * 0.05,
+                      width: width * 0.2,
+                      child: Center(child: Text('Data')),
+                      decoration: BoxDecoration(
+                        color: Colors.cyan,
+                        //borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+              Container(
+                height: height * 0.05,
+                width: width * 0.2,
+                child: Center(child: Text('Data')),
+                decoration: BoxDecoration(
+                  color: Colors.pinkAccent,
+                  //borderRadius: BorderRadius.circular(20),
                 ),
-                RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      //forwardAnimation();
-                    });
-                  },
-                  child: Icon(Icons.forward),
-                  color: kThemeColor,
-                ),
-              ],
-            ),
-            Container(
-              height: height * 0.3,
-            )
-          ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        //reverseAnimation();
+                      });
+                    },
+                    child: Icon(Icons.backspace_sharp),
+                    color: kThemeColor,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        //forwardAnimation();
+                      });
+                    },
+                    child: Icon(Icons.forward),
+                    color: kThemeColor,
+                  ),
+                ],
+              ),
+              Container(
+                height: height * 0.3,
+              )
+            ],
+          ),
         ),
       ),
     );
