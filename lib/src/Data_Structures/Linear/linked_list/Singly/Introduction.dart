94763a971c66655b0fb38f21c03c60f8c2ce5780
sdoshi983 <sdoshi983@rku.ac.in>
2020-12-10 17:04:23 +0530

    Singly linked list's introduction page completed!

--- lib/src/Data_Structures/Linear/linked_list/Singly/Introduction.dart
+++ lib/src/Data_Structures/Linear/linked_list/Singly/Introduction.dart
@@ -1,4 +1,5 @@
 import 'package:dsa_simulation/src/Utilities/widgets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:widget_arrows/widget_arrows.dart';
 
@@ -10,6 +11,45 @@ class SinglyIntroduction extends StatefulWidget {
 }
 
 class _SinglyIntroductionState extends State<SinglyIntroduction> {
+  double arrowOpacity = 0;
+  double dataOpacity = 0;
+  double headPointerOpacity = 0, headPointerArrowOpacity = 0;
+  double tailPointerOpacity = 0, tailPointerArrowOpacity = 0;
+  double animationState = 0;
+  double nullWidth = 0;
+
+  void forward() {
+    if (animationState == 0)
+      dataOpacity = 1;
+    else if (animationState == 1){
+      arrowOpacity = 1;
+      nullWidth = 0.07;
+    }
+    else if(animationState == 2)
+      headPointerOpacity = tailPointerOpacity = 1;
+    else if(animationState == 3)
+      headPointerArrowOpacity = 1;
+    else if(animationState == 4)
+      tailPointerArrowOpacity = 1;
+    animationState++;
+  }
+
+  void reverse() {
+    if(animationState == 5)
+      tailPointerArrowOpacity = 0;
+    else if(animationState == 4)
+      headPointerArrowOpacity = 0;
+    else if(animationState == 3)
+      headPointerOpacity = tailPointerOpacity = 0;
+    else if (animationState == 2) {
+      arrowOpacity = 0;
+      nullWidth = 0;
+    }
+    else if (animationState == 1) dataOpacity = 0;
+
+    animationState--;
+  }
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -26,7 +66,45 @@ class _SinglyIntroductionState extends State<SinglyIntroduction> {
                 'Introduction to Singly Linked List',
                 style: Theme.of(context).textTheme.headline6,
               ),
-              SizedBox(height: height * 0.05,),
+              SizedBox(
+                //height: height * 0.01,
+                //width: double.infinity,
+              ),
+              Row(
+                children: [
+                  SizedBox(width: width * 0.07,),
+                  ArrowElement(
+                    id: 'headPointer',
+                    targetId: 'first',
+                    sourceAnchor: Alignment.bottomCenter,
+                    targetAnchor: Alignment.topCenter,
+                    color: Colors.white.withOpacity(headPointerArrowOpacity),
+                    child: Text(
+                      'Head Pointer',
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(headPointerOpacity),
+                          fontSize: height * 0.02
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: width * 0.34,),
+                  ArrowElement(
+                    id: 'tailPointer',
+                    targetId: 'third',
+                    sourceAnchor: Alignment.bottomCenter,
+                    targetAnchor: Alignment.topCenter,
+                    color: Colors.white.withOpacity(tailPointerArrowOpacity),
+                    child: Text(
+                      'Tail Pointer',
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(tailPointerOpacity),
+                          fontSize: height * 0.02
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: height * 0.01,),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
@@ -34,27 +112,43 @@ class _SinglyIntroductionState extends State<SinglyIntroduction> {
                     id: 'first',
                     targetId: 'second',
                     sourceAnchor: Alignment.centerRight,
-                    //targetAnchor: Alignment.centerLeft,
-                    child: Container(
+                    color: Colors.white.withOpacity(arrowOpacity),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
                       height: height * 0.05,
                       width: width * 0.2,
-                      child: Center(child: Text('Data')),
+                      child: Center(
+                          child: Text(
+                        'Data',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(dataOpacity),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
                       decoration: BoxDecoration(
                         color: Colors.green,
                         //borderRadius: BorderRadius.circular(20),
                       ),
                     ),
                   ),
-
                   ArrowElement(
                     id: 'second',
                     targetId: 'third',
                     sourceAnchor: Alignment.centerRight,
-
-                    child: Container(
+                    color: Colors.white.withOpacity(arrowOpacity),
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 200),
                       height: height * 0.05,
                       width: width * 0.2,
-                      child: Center(child: Text('Data')),
+                      child: Center(
+                          child: Text(
+                        'Data',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(dataOpacity),
+                          fontWeight: FontWeight.bold,
+
+                        ),
+                      )),
                       decoration: BoxDecoration(
                         color: Colors.yellow,
                         //borderRadius: BorderRadius.circular(20),
@@ -63,35 +157,58 @@ class _SinglyIntroductionState extends State<SinglyIntroduction> {
                   ),
                   ArrowElement(
                     id: 'third',
-                    child: Container(
+                    targetId: 'null',
+                    sourceAnchor: Alignment.centerRight,
+                    //targetAnchor: Alignment.topCenter,
+                    color: Colors.white.withOpacity(arrowOpacity),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
                       height: height * 0.05,
                       width: width * 0.2,
-                      child: Center(child: Text('Data')),
+                      child: Center(
+                          child: Text(
+                        'Data',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(dataOpacity),
+                          fontWeight: FontWeight.bold,
+
+                        ),
+                      )),
                       decoration: BoxDecoration(
                         color: Colors.cyan,
                         //borderRadius: BorderRadius.circular(20),
                       ),
                     ),
                   ),
+                  ArrowElement(
+                    id: 'null',
+                    color: Colors.white.withOpacity(arrowOpacity),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      height: height * 0.05,
+                      width: width * nullWidth,
+                      child: Center(
+                          child: Text(
+                            'Null',
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(arrowOpacity),
+                              fontWeight: FontWeight.bold,
 
+                            ),
+                          )),
+
+                    ),
+                  ),
                 ],
               ),
-              Container(
-                height: height * 0.05,
-                width: width * 0.2,
-                child: Center(child: Text('Data')),
-                decoration: BoxDecoration(
-                  color: Colors.pinkAccent,
-                  //borderRadius: BorderRadius.circular(20),
-                ),
-              ),
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   RaisedButton(
                     onPressed: () {
                       setState(() {
-                        //reverseAnimation();
+                        reverse();
                       });
                     },
                     child: Icon(Icons.backspace_sharp),
@@ -100,7 +217,7 @@ class _SinglyIntroductionState extends State<SinglyIntroduction> {
                   RaisedButton(
                     onPressed: () {
                       setState(() {
-                        //forwardAnimation();
+                        forward();
                       });
                     },
                     child: Icon(Icons.forward),
@@ -118,4 +235,3 @@ class _SinglyIntroductionState extends State<SinglyIntroduction> {
     );
   }
 }
-
