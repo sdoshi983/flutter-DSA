afee883db27e0b621dadb10081cfd02c2b8c336f
sdoshi983 <sdoshi983@rku.ac.in>
2020-12-11 15:52:36 +0530

    Singly linked list's insertion page route added

--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -1,6 +1,7 @@
 import 'package:dsa_simulation/src/Data_Structures/Linear/Stack/Introduction.dart';
 import 'package:dsa_simulation/src/Data_Structures/Linear/Stack/stack_mainPage.dart';
 import 'package:dsa_simulation/src/Data_Structures/Linear/linked_list/Singly/Introduction.dart';
+import 'package:dsa_simulation/src/Data_Structures/Linear/linked_list/Singly/singly_insertion.dart';
 import 'package:dsa_simulation/src/Data_Structures/Linear/linked_list/Singly/singly_main_page.dart';
 import 'package:dsa_simulation/src/Data_Structures/Linear/linked_list/linked_list_main_page.dart';
 import 'package:dsa_simulation/src/data_structures/linear_and_non_linear.dart';
@@ -32,6 +33,8 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (_) => SinglyMainPage());
       case '/SinglyIntroductionPage':
         return MaterialPageRoute(builder: (_) => SinglyIntroduction());
+      case '/SinglyInsertionPage':
+        return MaterialPageRoute(builder: (_) => SinglyInsertion());
       default:
         // If there is no such named route in the switch statement, e.g. /third
         return MaterialPageRoute(
