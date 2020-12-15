62fbf7e0701efdec4bd7777e5555adc99f5b1631
sdoshi983 <sdoshi983@rku.ac.in>
2020-12-15 18:14:44 +0530

    Animation added to drawer items

--- lib/src/utilities/drawer_widget.dart
+++ lib/src/utilities/drawer_widget.dart
@@ -1,5 +1,6 @@
 import 'package:dsa_simulation/src/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:auto_animated/auto_animated.dart';
 
 class DrawerWidget extends StatefulWidget {
   @override
@@ -11,6 +12,70 @@ class _DrawerWidgetState extends State<DrawerWidget> {
   String networkImageUrl = "https://cdn.logo.com/hotlink-ok/logo-social.png";
   bool dataStructures = false;
 
+
+
+  List<ReusableExpansionPanelList> mainList = [
+    ReusableExpansionPanelList(
+      text: "Data Structures",
+      isExpanded: false,
+      routeName: "/LinearNonLinearPage",
+      alignment: -1,
+      child: LiveListOptions(itemCount: 2, list: dataStructuresList,),
+    ),
+    ReusableExpansionPanelList(
+      text: "Algorithms",
+      isExpanded: false,
+      alignment: -1,
+      child: Container(),
+    )
+  ];
+
+  static List<ReusableExpansionPanelList> dataStructuresList = [
+    ReusableExpansionPanelList(
+      text: "  Linear",
+      isExpanded: false,
+      routeName: "/LinearDS",
+      alignment: -0.7,
+      child: LiveListOptions(itemCount: 4, list: linearList,),
+    ),
+    ReusableExpansionPanelList(
+      text: "  Non Linear",
+      isExpanded: false,
+      alignment: -0.7,
+      child: Container(),
+    ),
+  ];
+
+  static List<ReusableExpansionPanelList> linearList = [
+    ReusableExpansionPanelList(
+      text: "    Array",
+      isExpanded: false,
+      child: Container(),
+      alignment: -0.5,
+      routeName: "/ArrayPageView",
+    ),
+    ReusableExpansionPanelList(
+      text: "    Stack",
+      isExpanded: false,
+      routeName: '/StackMainPage',
+      alignment: -0.5,
+      child: Container(),
+    ),
+    ReusableExpansionPanelList(
+      text: "    Queue",
+      isExpanded: false,
+      alignment: -0.5,
+      child: Container(),
+    ),
+    ReusableExpansionPanelList(
+      text: "    Linked List",
+      isExpanded: false,
+      alignment: -0.5,
+      child: Container(),
+    ),
+  ];
+  
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -18,11 +83,13 @@ class _DrawerWidgetState extends State<DrawerWidget> {
 
     return Drawer(
       child: Container(
+        height: height,
         color: Colors.black,
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15),
           child: SingleChildScrollView(
-            child: Column(
+
+            child: Column(  
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(height: height * 0.07),
@@ -64,70 +131,9 @@ class _DrawerWidgetState extends State<DrawerWidget> {
                   color: Colors.white,
                 ),
                 SizedBox(
-                  height: height * 0.04,
+                  height: height * 0.02,
                 ),
-                ReusableExpansionPanelList(
-                  text: "Data Structures",
-                  isExpanded: false,
-                  routeName: "/LinearNonLinearPage",
-                  alignment: -1,
-                  child: Column(
-                    children: [
-                      ReusableExpansionPanelList(
-                        text: "Linear",
-                        isExpanded: false,
-                        routeName: "/LinearDS",
-                        alignment: -0.7,
-                        child: Column(
-                          children: [
-                            ReusableExpansionPanelList(
-                              text: "Array",
-                              isExpanded: false,
-                              child: Container(),
-                              alignment: -0.5,
-                              routeName: "/ArrayPageView",
-                            ),
-                            ReusableExpansionPanelList(
-                              text: "Stack",
-                              isExpanded: false,
-                              routeName: '/StackMainPage',
-                              alignment: -0.5,
-
-                              child: Container(),
-                            ),
-                            ReusableExpansionPanelList(
-                              text: "Queue",
-                              isExpanded: false,
-                              alignment: -0.5,
-
-                              child: Container(),
-                            ),
-                            ReusableExpansionPanelList(
-                              text: "Linked List",
-                              isExpanded: false,
-                              alignment: -0.5,
-
-                              child: Container(),
-                            ),
-                          ],
-                        ),
-                      ),
-                      ReusableExpansionPanelList(
-                        text: "Non Linear",
-                        isExpanded: false,
-                        alignment: -0.7,
-                        child: Container(),
-                      ),
-                    ],
-                  ),
-                ),
-                ReusableExpansionPanelList(
-                  text: "Algorithms",
-                  isExpanded: false,
-                  alignment: -1,
-
-                  child: Container(),
-                )
+                SingleChildScrollView(child: LiveListOptions(itemCount: 2, list: mainList,)),
               ],
             ),
           ),
@@ -138,7 +144,8 @@ class _DrawerWidgetState extends State<DrawerWidget> {
 }
 
 class ReusableExpansionPanelList extends StatefulWidget {
-  ReusableExpansionPanelList({this.isExpanded, this.text, this.child, this.routeName, this.alignment});
+  ReusableExpansionPanelList(
+      {this.isExpanded, this.text, this.child, this.routeName, this.alignment});
   bool isExpanded;
   Widget child;
   String text, routeName;
@@ -157,7 +164,7 @@ class _ReusableExpansionPanelListState
     return Theme(
       data: Theme.of(context).copyWith(cardColor: Colors.black),
       child: GestureDetector(
-        onTap: (){
+        onTap: () {
           Navigator.pushNamed(context, widget.routeName);
         },
         child: ExpansionPanelList(
@@ -170,12 +177,11 @@ class _ReusableExpansionPanelListState
             ExpansionPanel(
               headerBuilder: (BuildContext context, bool isExpanded) {
                 return Align(
-                  alignment: Alignment(widget.alignment, 0),
+                  alignment: Alignment(-1, 0),
                   child: Text(
                     widget.text,
-                    style:
-                        TextStyle(color: Colors.white, fontSize: height * 0.027
-                        ),
+                    style: TextStyle(
+                        color: Colors.white, fontSize: height * 0.027),
                   ),
                 );
               },
@@ -186,5 +192,59 @@ class _ReusableExpansionPanelListState
         ),
       ),
     );
+  }
+}
+
+class LiveListOptions extends StatefulWidget {
+  LiveListOptions({this.itemCount, this.list});
+  List<ReusableExpansionPanelList> list;
+  int itemCount;
+  @override
+  _LiveListOptionsState createState() => _LiveListOptionsState();
+}
+
+class _LiveListOptionsState extends State<LiveListOptions> {
+
+  final options = LiveOptions(
+    // Start animation after (default zero)
+    delay: Duration(milliseconds: 1),
+
+    // Show each item through (default 250)
+    showItemInterval: Duration(milliseconds: 100),
+
+    // Animation duration (default 250)
+    showItemDuration: Duration(milliseconds: 500),
+
+    // Animations starts at 0.05 visible
+    // item fraction in sight (default 0.025)
+    visibleFraction: 0.05,
+
+    // Repeat the animation of the appearance
+    // when scrolling in the opposite direction (default false)
+    // To get the effect as in a showcase for ListView, set true
+    reAnimateOnVisibility: false,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return LiveList.options(
+        shrinkWrap: true,
+        options: options,
+        itemCount: widget.itemCount,
+        itemBuilder: (BuildContext context, int index,
+            Animation<double> animation) {
+          return FadeTransition(
+            opacity: Tween<double>(
+              begin: 0,
+              end: 1,
+            ).animate(animation),
+            child: SlideTransition(
+                position: Tween<Offset>(
+                  begin: Offset(0, -0.1),
+                  end: Offset.zero,
+                ).animate(animation),
+                child: widget.list[index]),
+          );
+        });
   }
 }
