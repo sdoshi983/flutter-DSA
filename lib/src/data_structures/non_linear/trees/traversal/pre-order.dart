64bb1e9669a2fd8ea9d99dbd43af1ba44229596a
smit-mist <smit3355mistry@gmail.com>
2021-01-21 15:38:40 +0530

    Started Pre order

--- lib/src/data_structures/non_linear/trees/traversal/pre-order.dart
+++ lib/src/data_structures/non_linear/trees/traversal/pre-order.dart
@@ -2,8 +2,10 @@ import 'package:dsa_simulation/src/utilities/address_maninter.dart';
 import 'package:dsa_simulation/src/utilities/base_template.dart';
 import 'package:dsa_simulation/src/utilities/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:widget_arrows/widget_arrows.dart';
 
 import '../../../../constants.dart';
+import '../tree_introduction.dart';
 
 class PreOrderTraversal extends StatefulWidget {
   @override
@@ -11,14 +13,14 @@ class PreOrderTraversal extends StatefulWidget {
 }
 
 class _PreOrderTraversalState extends State<PreOrderTraversal> {
+  double tipLength = 5;
   @override
   Widget build(BuildContext context) {
-
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     print('Build Method Starts');
     print(path);
-    path = ['Home', 'DS', 'Trees','Traversal','Pre-Order'];
+    path = ['Home', 'DS', 'Trees', 'Traversal', 'Pre-Order'];
     print(path);
     print('build method ends');
     return WillPopScope(
@@ -42,9 +44,47 @@ class _PreOrderTraversalState extends State<PreOrderTraversal> {
             backgroundColor: kThemeColor,
             iconTheme: IconThemeData(color: Colors.white),
             title: Center(
-                child: Container(width: width * 0.9, height: 30, child: AddressBar())),
+                child: Container(
+                    width: width * 0.9, height: 30, child: AddressBar())),
           ),
-          body: Container(height: height*0.9,width: width,),
+          body: Container(
+            height: height*0.9,
+            width: width,
+            color: Colors.black,
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Pre-Order Tree Traversal',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(color: Colors.white),
+                  ),
+                  Container(
+                    height: height*0.6,
+                    width: width,
+                    color: Colors.greenAccent,
+                    child: Stack(
+                      children: [
+                        AnimatedContainer(
+                          duration: Duration(milliseconds: 500),
+                          height: width*0.12,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+
+                            shape: BoxShape.circle,
+                          ),
+                        )
+                      ],
+
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
