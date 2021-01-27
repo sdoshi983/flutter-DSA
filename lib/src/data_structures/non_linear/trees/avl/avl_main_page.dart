2d08d217a24aaa6c390fe7f9c0bd37547092e3df
sdoshi983 <sdoshi983@rku.ac.in>
2021-01-27 15:19:50 +0530

    AVL main page completed

---
+++ lib/src/data_structures/non_linear/trees/avl/avl_main_page.dart
@@ -0,0 +1,88 @@
+import 'package:dsa_simulation/src/Utilities/address_maninter.dart';
+import 'package:dsa_simulation/src/Utilities/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:dsa_simulation/src/utilities/base_template.dart';
+
+import '../../../../constants.dart';
+
+
+class AvlMainPage extends StatefulWidget {
+  @override
+  _AvlMainPageState createState() => _AvlMainPageState();
+}
+
+class _AvlMainPageState extends State<AvlMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    path = ['Home', 'DS', 'Trees','AVL'];
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pushNamedAndRemoveUntil(
+            context, '/TreeMain', (route) => false);
+        return true;
+      },
+      child: BaseTemplate(
+        body: Scaffold(
+          appBar: AppBar(
+            leading: ClipRect(
+              child: MaterialButton(
+                child: Icon(Icons.menu_rounded),
+                onPressed: () {
+                  toggle();
+                },
+                splashColor: Colors.black,
+              ),
+            ),
+            backgroundColor: kThemeColor,
+            iconTheme: IconThemeData(color: Colors.white),
+            title: Center(
+                child: Container(width: width * 0.9, height: 30, child: AddressBar())),
+          ),
+          body: Container(
+            height: height,
+            width: width,
+            color: Colors.black,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: height * 0.05,
+                ),
+                Tiles(
+                  onPress: () {
+                    addElement('Intro');
+                    Navigator.of(context).pushNamed('/AvlIntroduction');
+                  },
+                  title: 'Introduction',
+                ),
+                SizedBox(
+                  height: height * 0.02,
+                ),
+                Tiles(
+                  onPress: () {
+                    addElement('Insertion');
+                    Navigator.of(context).pushNamed('/BSTInsertion');
+                  },
+                  title: 'Insertion',
+                ),
+                SizedBox(
+                  height: height * 0.02,
+                ),
+                Tiles(
+                  onPress: () {
+                    addElement('Deletion ');
+                    Navigator.of(context).pushNamed('/BSTDeletion');
+                  },
+                  title: 'Deletion',
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
